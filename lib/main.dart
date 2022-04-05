bf1746e2339ca5c7e083e04993e725529f19ff52
Dogukan Balaman <dodikbalaman@hotmail.com>
2022-04-05 23:49:18 +0300

    First

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,112 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, unused_element
+
 import 'package:flutter/material.dart';
+// Scaffold, Buton ve AppBar için gerekli kaynaklar.
+// Flutter bu paket ile geliyor. Genel olarak geliştiriciler işlerini kolaylaştırmak ve hızlandırmak için paketler kullanırlar. bkz. https://pub.dev/
+
+//void main() => runApp(
+//    MaterialApp(home: Todo()), //Material'dan gelen 1. class
+//); //MyApp verirse main'i çalıştır, vermez ise çalıştırma
 
 void main() {
-  runApp(const MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class App extends StatelessWidget {
+//StatelessWidget sadece AppBar gibi yeri asla değişmeyecek yerlerde kullanılır. Bizim listemiz sürekli değişir.
+//Telefonun modeline göre ekranın üst tarafı kamera yüzünden değişebilir fakat bunu şu anlık ayarlamayacağım.
   @override
   Widget build(BuildContext context) {
+    //Flutter Widget'ları uygulamanın o anki durumuna göre nasıl gözükeceğini belirler.
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home'),
-    );
+        //Tüm kod MaterialApp'ın içerdiği bir widget gibi düşünülebilir.
+        //Material'dan gelen class
+        title: 'Yapılacaklar Listesi',
+        home: TodoList());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class TodoList extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _TodoListState createState() => _TodoListState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _TodoListState extends State<TodoList> {
+  //veri kaydetme
+  final List<String> _todoList = <String>[];
+  // Text yazma yeri
+  final TextEditingController _textFieldController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // Uygulama düzeni
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
+      appBar: AppBar(title: const Text('Yapılacaklar Listesi')),
+      body: ListView(children: _getItems()),
+      // Listeye ekleme yapma
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          onPressed: () => _displayDialog(context),
+          tooltip: 'Görev Ekleme',
+          child: Icon(Icons.add)),
     );
+  }
+
+// Listeye veri ekleme
+  void _addTodoItem(String title) {
+    // Değişim olursa setState bildirecek
+    setState(() {
+      _todoList.add(title);
+    });
+    _textFieldController.clear();
+  }
+
+//listeyi doldurma
+  Widget _buildTodoItem(String title) {
+    return ListTile(
+      title: Text(title),
+    );
+  }
+
+//Bir widget
+// ismini sorma dialogu
+  Future<Future> _displayDialog(BuildContext context) async {
+    //isim sorma için state değiştirtme
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Listeye ekleme yap'),
+            content: TextField(
+              controller: _textFieldController,
+              decoration: const InputDecoration(hintText: 'Ekleme Yazın'),
+            ),
+            actions: <Widget>[
+              //ekleme butonu
+              TextButton(
+                child: const Text('Ekle'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _addTodoItem(_textFieldController.text);
+                },
+              ),
+              //iptal butonu
+              TextButton(
+                child: const Text('İptal'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
+  }
+
+// Tekrarlama
+  List<Widget> _getItems() {
+    final List<Widget> _todoWidgets = <Widget>[];
+    for (String title in _todoList) {
+      _todoWidgets.add(_buildTodoItem(title));
+    }
+    return _todoWidgets;
   }
 }
